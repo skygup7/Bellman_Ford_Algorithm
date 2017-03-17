61d46fc8b5ab5553cdd5fc57489b5e44aef489b8
Akash Gupta <sky.gup7@gmail.com>
2017-03-17 16:57:56 +0530

    Initialization

---
+++ Code.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define LLMAX 1000000000000000010
+#define MAX 100005
+#define gcd __gcd
+#define mod 1000000007
+#define pb(n) push_back(n)
+#define fr front()
+#define vint vector<int>
+#define vstring vector<string>
+#define vll vector<long long int>
+#define pii pair<int,int>
+#define forp(i,a,n) for(int i=(a);i<(n);i++)
+#define forn(i,a,n) for(int i=(a);i>=(n);i--)
+#define mp std::map<string, int>
+#define csl ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+struct edge{
+    int src;
+    int dest;
+    int weight;
+};
+struct graph{
+    int v;
+    int e;
+    struct edge* edges;
+};
+
+int main()
+{
+    csl;
+    
+}
+
