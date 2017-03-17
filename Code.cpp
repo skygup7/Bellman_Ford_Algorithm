fb2fbd89a9c409bb3e8c077155b045fb0e3884e5
Akash Gupta <sky.gup7@gmail.com>
2017-03-17 19:02:59 +0530

    Declaration of Class

--- Code.cpp
+++ Code.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define LLMAX 1000000000000000010
 #define MAX 100005
 #define gcd __gcd
-#define mod 1000000007
+#define INF 1000000007
 #define pb(n) push_back(n)
 #define fr front()
 #define vint vector<int>
@@ -16,20 +16,43 @@ using namespace std;
 #define mp std::map<string, int>
 #define csl ios_base::sync_with_stdio(false); cin.tie(NULL)
 
-struct edge{
-    int src;
-    int dest;
-    int weight;
-};
-struct graph{
-    int v;
-    int e;
-    struct edge* edges;
+
+class graph
+{
+    int u;
+    list<pii > * adj;
+    int * dist;
+public:
+    graph(int v);
+    void addedge(int src,int dest,int w);
+    void BellmanFord(int src);
 };
 
-int main()
+graph::graph(int v)
 {
-    csl;
+    u=v;
+    adj=new list<pii >[v];
+    dist=new int[v];
+    for(int i=0;i<u;i++)
+        dist[i]=INF;
+}
+void graph::addedge(int src,int dest,int w)
+{
+    adj[src].pb(make_pair(dest,w));
+}
+void graph::BellmanFord(int src)
+{
+    dist[src]=0;
     
 }
 
+int main()
+{
+    graph g(4);
+    g.addedge(0,1,1);
+    g.addedge(0,2,1);
+    g.addedge(1,2,1);
+    g.addedge(2,0,1);
+    g.addedge(2,3,1);
+    g.addedge(3,3,1);
+}
